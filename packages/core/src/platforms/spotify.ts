975ba62918d4ef98e8ab4f2e640dcf2d7be9bf1a
N1KHIL-CHOUDHARY <nikhil2k7h@gmail.com>
2025-12-20 23:36:27 +0530

    feat: expanded the spotify handler for tracks playlist artist abum

--- packages/core/src/platforms/spotify.ts
+++ packages/core/src/platforms/spotify.ts
@@ -1,15 +1,16 @@
 import { DeepLinkHandler } from '../types';
 
 export const spotifyHandler: DeepLinkHandler = {
-  match: (url) => url.match(/open.spotify\.com\/track\/([^/?]+)/),
+  match: (url) => url.match(/open\.spotify\.com\/(track|artist|album|playlist)\/([^/?#]+)/),
 
   build: (webUrl, match) => {
-    const songId = match[1];
+    const type = match[1];
+    const id = match[2];
 
     return {
       webUrl,
-      ios: `spotify://track/${songId}`,
-      android: `intent://track/${songId}#Intent;scheme=spotify;package=com.spotify.music;end`,
+      ios: `spotify://${type}/${id}`,
+      android: `intent://${type}/${id}#Intent;scheme=spotify;package=com.spotify.music;end`,
       platform: 'spotify',
     };
   },
